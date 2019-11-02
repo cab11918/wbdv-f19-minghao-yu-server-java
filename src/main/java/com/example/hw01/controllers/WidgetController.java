3fb63e4dd05f925d96c534fdd3da8ae489e45193
cab11918 <yu.mingh@husky.neu.edu>
2019-11-01 21:45:31 -0400

    test

--- src/main/java/com/example/hw01/controllers/WidgetController.java
+++ src/main/java/com/example/hw01/controllers/WidgetController.java
@@ -22,6 +22,7 @@ public class WidgetController {
     Widget w1 = new Widget(123, "widget1", 0, "text1", "source1", 1, type.HEADING);
     Widget w2 = new Widget(234, "widget2", 1, "text2", "source2", 1, type.PARAGRAPH);
     Widget w3 = new Widget(345, "widget3", 2, "text3", "source3", 1, type.LIST);
+    
     Widget w4 = new Widget(456, "widget4", 3, "text4", "source4", 1, type.IMAGE);
     Widget w5 = new Widget(567, "widget5", 4, "text5", "source5", 1, type.LINK);
 
