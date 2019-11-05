4c9fad2c6bc1cc532d2bb44119e11ded841413b9
cab11918 <yu.mingh@husky.neu.edu>
2019-11-04 21:06:21 -0500

    server final version

--- src/main/java/com/example/hw01/CourseService.java
+++ src/main/java/com/example/hw01/CourseService.java
@@ -1,5 +1,6 @@
 package com.example.hw01;
 
+import com.example.hw01.controllers.CourseController;
 import com.example.hw01.models.Course;
 import com.example.hw01.models.Lesson;
 import com.example.hw01.models.Topic;
@@ -39,6 +40,26 @@ public class CourseService {
 
   public List<Course> getCourses() {
     return this.courses;
+  }
+
+  public List<Course> createCourse(Course c) {
+    return new CourseController().createCourse(c);
+  }
+
+  public List<Course> findAllCourses() {
+    return new CourseController().findAllCourses();
+  }
+
+  public Course findCourseById(int id) {
+    return new CourseController().findCourseById(id);
+  }
+
+  public List<Course> updateCourse(int id, Course c) {
+    return new CourseController().updateCourse(id, c);
+  }
+
+  public List<Course> deleteCourse(int id) {
+    return new CourseController().deleteCourse(id);
   }
 
 }
