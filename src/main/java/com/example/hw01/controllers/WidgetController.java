620600cb393884a7284dfe43389335cb592f46f4
cab11918 <yu.mingh@husky.neu.edu>
2019-11-04 22:22:17 -0500

    mighty try

--- src/main/java/com/example/hw01/controllers/WidgetController.java
+++ src/main/java/com/example/hw01/controllers/WidgetController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials =
+@CrossOrigin(origins =  "https://infinite-citadel-93794.herokuapp.com", allowCredentials =
     "true",
     allowedHeaders = "*")
 @RestController
